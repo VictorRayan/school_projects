8fe470928578fc8942274a5e2c43780f99c1c323
Victor Rayan <victorrayansouzaramos@gmail.com>
2022-03-06 20:26:27 -0300

    Added style to better log vizualization, and added other data input log from user

--- contact_us_log.php
+++ contact_us_log.php
@@ -1,4 +1,5 @@
 <?php
+
 echo "<h1>Client or User Log Data from contactus.html</h1>";
 $txtName = $_POST['txtName'];
 $inpBorn = $_POST['inpBorn'];
@@ -6,11 +7,28 @@ $txtEmail = $_POST['txtEmail'];
 $txtTel = $_POST['txtTel'];
 $txtObs = $_POST['txtObs'];
 $ckNewslatter = $_POST['ckNewslatter'];
+$optCar = $_POST['optCar'];
+
 
 echo "<br>Name: $txtName";
 echo "<br>Email: $txtEmail";
 echo "<br>Tel: $txtTel";
 echo "<br>Birthday: $inpBorn";
+
+switch($optCar){
+
+    case 'audi':
+        echo "<br>Selected Car: Audi<br><img src='style/audi.jpg' style='max-width:40vw; max-height:30vh;'>";
+        break;
+    case 'fiat':
+        echo "<br>Selected Car: Fiat<br><img src='style/fiat.jpg' style='max-width:40vw; max-height:30vh;'>";
+        break;
+    case 'tesla':
+        echo "<br>Selected Car: Tesla<br><img src='style/tesla_model.jpg' style='max-width:40vw; max-height:30vh;'>";
+        break;
+}
+echo "<br>";
+
 
 /*Verify if the checkbox is set or non-default, by definition or non-default, when the option is not variable,
  set in reverse of the variables, and yes there is a mark of this option, which is not variable.*/
